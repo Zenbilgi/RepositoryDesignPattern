57a1ecc0b6b6f61a3aaf712e7ee486b209376a66
Mehmet TAS <>
2023-12-29 01:27:19 +0100

    The code has been indented for better readability.

--- RepositoryPatternExample/Models/Address.cs
+++ RepositoryPatternExample/Models/Address.cs
@@ -4,12 +4,12 @@ public class Address
 {
     public int Id { get; set; }
 
-	public string Country { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
 
-	public string Description { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
-	public int CustomerId { get; set; }
+    public int CustomerId { get; set; }
 
     public Customer? Customer { get; set; }
-}
+    }
 
--- RepositoryPatternExample/Models/Customer.cs
+++ RepositoryPatternExample/Models/Customer.cs
@@ -3,10 +3,15 @@
 public class Customer
 {
 	public int Id { get; set; }
+
 	public string Name { get; set; } = string.Empty;
+
 	public string FirstName { get; set; } = string.Empty;
+
 	public string LastName { get; set; } = string.Empty;
+
 	public string Place { get; set; } = string.Empty;
+
 	public List<Address>? Address { get; set; }
 
 }
